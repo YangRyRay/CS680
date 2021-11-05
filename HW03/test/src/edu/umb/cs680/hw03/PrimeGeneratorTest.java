91692d32a690df238400a7e27dfa466f469fb5f2
YangRyRay <78864268+YangRyRay@users.noreply.github.com>
2021-11-05 17:38:39 -0400

    Add files via upload

---
+++ HW03/test/src/edu/umb/cs680/hw03/PrimeGeneratorTest.java
@@ -0,0 +1,35 @@
+package edu.umb.cs680.hw03;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class PrimeGeneratorTest {
+    @Test
+    public void primeGen1to10() {
+        PrimeGenerator gen = new PrimeGenerator(1,10);
+        gen.generatePrimes();
+        Object[] expected = {2L,3L, 5L,7L};
+        assertArrayEquals(expected,gen.getPrimes().toArray());
+    }
+    @Test
+    public void primeGenNeg10to10() {
+        try{
+            PrimeGenerator gen = new PrimeGenerator(-10,10);
+            fail("Negative index Allowed");
+        }
+        catch(RuntimeException e){
+            assertEquals("Wrong input values: from=-10 to=10",e.getMessage());
+        }
+    }
+    @Test
+    public void primeGen100to1() {
+        try{
+            PrimeGenerator gen = new PrimeGenerator(100,1);
+            fail("First index larger allowed");
+        }
+        catch(RuntimeException e){
+            assertEquals("Wrong input values: from=100 to=1",e.getMessage());
+        }
+    }
+
+}
